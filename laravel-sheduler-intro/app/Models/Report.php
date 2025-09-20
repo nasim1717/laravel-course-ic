594313f5eb1e00840305e00279885346521944a5
nasim1717 <nasimreza1717@gmail.com>
2025-09-20 17:44:52 +0600

    laravel-sheduler-intro

--- laravel-sheduler-intro/app/Models/Report.php
+++ laravel-sheduler-intro/app/Models/Report.php
@@ -1,9 +1,10 @@
 <?php
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Post extends Model
+class Report extends Model
 {
-    protected $guarded = [];
+    protected $fillable = ['name','status'];
 }
